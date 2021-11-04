2d970a502f6bb419109ecb59e868e9ea74ad1742
Jeremy <j1lin0101@gmail.com>
2021-11-04 14:50:28 -0700

    Jquery implementation into form

--- scripts/form.js
+++ scripts/form.js
@@ -1,22 +1,13 @@
-const form = document.getElementById('checkout');
+$(function () {
+    $('form').submit(function (event) {
+        event.preventDefault();
+        const values = $(this).serializeArray();
+        console.log(values);
+        for (let i = 0; i < values.length; i += 1) {
+            const name = values[i].name;
+            const value = values[i].value;
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const formInputs = document.getElementsByTagName('input');
-    console.log(formInputs);
-    for (let i = 0; i < formInputs.length - 1; i += 1) {
-        if (formInputs[i].getAttribute('type') == 'radio') {
-            if (!formInputs[i].checked) {
-                continue;
-            }
+            console.log(name + " is: " + value);
         }
-
-        console.log(formInputs[i].name + " is: " + formInputs[i].value);
-    }
-
-    const formSelect = document.getElementsByTagName('select')[0];
-    console.log(formSelect.name + " is: " + formSelect.value);
-
-    alert('Thank you. The form information has been received.');
+    })
 });
